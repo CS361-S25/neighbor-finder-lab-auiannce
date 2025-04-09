d426cbb4251eab2753599e65fb896102cfa4ff36
Aviannce Euwing <aviannceeuwing@Aviannces-MacBook-Air-2.local>
2025-04-09 09:36:13 -0500

    Started Project

--- NFAnimate.cpp
+++ NFAnimate.cpp
@@ -4,10 +4,29 @@
 emp::web::Document doc{"target"};
 
 class NFAnimator : public emp::web::Animate {
+    emp::web::Canvas canvas{100, 100, "canvas"};
+    
+    public: 
 
+     NFAnimator() {
+         doc << canvas;
+         for(int x = 0; x < 10; x++) {
+            for(int y = 0; y < 10; y++) {
+                canvas.Rect(x*10, y*10, 10, 10, "white", "black");
+            }
+         }
+
+     }
+     void FindNeighbors(x, y){
+        
+     }
 };
 
+
+
 NFAnimator animator;
+
+
 
 int main() {
     animator.Step();
